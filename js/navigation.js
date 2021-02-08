c07b4e54f8f1bbacaf9b6c1b64384dd3c9362516
mgmgmg123123 <mgmgmg123123.mogu@gmail.com>
2021-02-09 01:30:21 +0900

    ヘッダーおよびメニューの実装完了

---
+++ js/navigation.js
@@ -0,0 +1,34 @@
+function SwitchNavigation(){
+    let isMenuActive = document.getElementById("mobile-menu").classList.contains("mobile-menu__active");
+
+    if(isMenuActive){
+        document.getElementById("mobile-menu").classList.remove("mobile-menu__active");
+        document.getElementById("mobile-menu").classList.add("mobile-menu__inactive");
+        document.getElementById("header-navigation-mobile--button--top").classList.remove("header-navigation-mobile--button--top__close");
+        document.getElementById("header-navigation-mobile--button--middle").classList.remove("header-navigation-mobile--button--middle__close");
+        document.getElementById("header-navigation-mobile--button--bottom").classList.remove("header-navigation-mobile--button--bottom__close");
+    }else{
+        document.getElementById("mobile-menu").classList.remove("mobile-menu__inactive");
+        document.getElementById("mobile-menu").classList.add("mobile-menu__active");
+        document.getElementById("header-navigation-mobile--button--top").classList.add("header-navigation-mobile--button--top__close");
+        document.getElementById("header-navigation-mobile--button--middle").classList.add("header-navigation-mobile--button--middle__close");
+        document.getElementById("header-navigation-mobile--button--bottom").classList.add("header-navigation-mobile--button--bottom__close");
+    }
+}
+const inPageLinkElements = document.querySelectorAll('a[href^="#"]');
+for(let i =0; i < inPageLinkElements.length; i++){
+  inPageLinkElement = inPageLinkElements[i];
+  inPageLinkElement.addEventListener("click",event => {
+    event.preventDefault();
+    const speed = 1200;          // スクロールスピード   
+    const headerHeight = document.getElementById("header").clientHeight;
+    const hrefValue = event.currentTarget.getAttribute('href');
+    const targetElement = document.getElementById(hrefValue.replace('#', ''));
+    const destinationCoordinates = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+    $("html, body").animate({scrollTop:destinationCoordinates}, speed, "swing");
+    });
+};
+document.getElementById("header-navigation-mobile").addEventListener("click",event => {
+  event.preventDefault();
+  SwitchNavigation();
+});
