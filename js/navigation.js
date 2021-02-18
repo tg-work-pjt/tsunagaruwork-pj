9a0063d08ae7decefa1b965d3259de67fbc2368b
mgmgmg123123 <mgmgmg123123.mogu@gmail.com>
2021-02-19 03:30:53 +0900

    指摘取り込み2

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,8 @@
 class SwitchNavigation{
   constructor(){
       this.object= {};
-      this.object.button = document.getElementById("header-navigation-mobile")
-      this.object.menu = document.getElementById("mobile-menu")
+      this.object.button = document.querySelector("#header-navigation-mobile")
+      this.object.menu = document.querySelector("#mobile-menu")
       this.eventType = window.ontouchstart ? 'touchstart':'click';
       this. _addEventListeners();
   }
@@ -13,11 +13,11 @@ class SwitchNavigation{
   _toggleInactiveStyle(){
     //初回にmobile-menu_inactiveをhtmlに付与したままだと、ページ表示時にメニューが消えるアニメーションが見えてしまうため、
     //toggleとは別途実装する。
-    let isMenuActive = document.getElementById("mobile-menu").classList.contains("mobile-menu_active");
+    let isMenuActive = document.querySelector("#mobile-menu").classList.contains("mobile-menu_active");
     if(isMenuActive){
-      document.getElementById("mobile-menu").classList.remove("mobile-menu_inactive");
+      document.querySelector("#mobile-menu").classList.remove("mobile-menu_inactive");
     }else{
-        document.getElementById("mobile-menu").classList.add("mobile-menu_inactive");
+        document.querySelector("#mobile-menu").classList.add("mobile-menu_inactive");
     }
   }
   _addEventListeners(){
@@ -29,19 +29,26 @@ class SwitchNavigation{
 const switchNavigationObject = new SwitchNavigation(); 
 const inPageLinkElements = document.querySelectorAll('a[href^="#"]');
 inPageLinkElements.forEach(inPageLinkElement => {
-  inPageLinkElement.addEventListener("click",event => {
-    event.preventDefault();
-    // スクロールスピード   
-    const speed = 1200;          
-    const headerHeight = document.getElementById("header").clientHeight;
-    const hrefValue = event.currentTarget.getAttribute('href');
-    const targetElement = document.getElementById(hrefValue.replace('#', ''));
-    const destinationCoordinates = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-    $("html, body").animate({scrollTop:destinationCoordinates}, speed, "swing");
+    inPageLinkElement.addEventListener("click",event => {
+      SmoothScroll(event)
+    });
+    inPageLinkElement.addEventListener("touch",event => {
+      SmoothScroll(event)
     });
 });
 
-const mobileMenuList = document.getElementById("mobile-menu-list").children;
+function SmoothScroll(event){
+  event.preventDefault();
+  // スクロールスピード
+  const speed = 1200;
+  const headerHeight = document.querySelector("#header").clientHeight;
+  const hrefValue = event.currentTarget.getAttribute("href");
+  const targetElement = document.getElementById(hrefValue.replace("#", ""));
+  const destinationCoordinates = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+  $("html, body").animate({scrollTop:destinationCoordinates}, speed, "swing");
+}
+
+const mobileMenuList = document.querySelector("#mobile-menu-list").children;
 Array.prototype.forEach.call(mobileMenuList, function(mobileMenuItem) {
   mobileMenuItem.firstElementChild.addEventListener(switchNavigationObject.eventType,() => {
   switchNavigationObject._toggle();
